601a77832fbc6d77cdf3dea349ceb9ce7c5d2f20
Prakhar1808 <prakharsharma.home@gmail.com>
2026-01-05 22:06:13 +0530

    day 1 in javascript land

---
+++ day1.js
@@ -0,0 +1,3 @@
+console.log("day 1 in javascript!");
+let bot = "freecodecamp.org's website takes too long to load, i started a course there but the site doesn't load anymore";
+console.log(bot + ".");
