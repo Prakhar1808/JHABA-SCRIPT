a6c53ffb861585376b34a1650f204c59c39b1574
Prakhar1808 <prakharsharma.home@gmail.com>
2026-01-12 23:05:27 +0530

    Day8_lastCommits

---
+++ day8_arraysSomethingNew.js
@@ -0,0 +1,7 @@
+let fruits = ["apple", "banana", "orange"];
+
+let [first, second, third] = fruits;
+
+console.log(first);  // "apple"
+console.log(second); // "banana"
+console.log(third);  // "orange"
